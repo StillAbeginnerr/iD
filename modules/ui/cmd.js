959399e8bda8f38f1c194d8460dad44b3e800db8
shivam agarwal <88205668+StillAbeginnerr@users.noreply.github.com>
2024-12-01 00:51:28 +0530

    Fix for issue #4090: cmd.js line 20-26 replaced with OS-specific keyboard shortcut mappings with localization

--- modules/ui/cmd.js
+++ modules/ui/cmd.js
@@ -17,12 +17,11 @@ export var uiCmd = function (code) {
     var mac = (detected.os === 'mac');
     var result = '',
         replacements = {
-            '⌘': mac ? '⌘ ' + t('shortcuts.key.cmd')    : t('shortcuts.key.ctrl'),
-            '⇧': mac ? '⇧ ' + t('shortcuts.key.shift')  : t('shortcuts.key.shift'),
-            '⌥': mac ? '⌥ ' + t('shortcuts.key.option') : t('shortcuts.key.alt'),
-            '⌃': mac ? '⌃ ' + t('shortcuts.key.ctrl')   : t('shortcuts.key.ctrl'),
-            '⌫': mac ? '⌫ ' + t('shortcuts.key.delete') : t('shortcuts.key.backspace'),
-            '⌦': mac ? '⌦ ' + t('shortcuts.key.del')    : t('shortcuts.key.del'),
+            '⌘': mac ?  t('shortcuts.key.cmd')    : t('shortcuts.key.ctrl'),
+            '⇧': mac ? t('shortcuts.key.shift')  : t('shortcuts.key.shift'),
+            '⌥': mac ?  t('shortcuts.key.option') : t('shortcuts.key.alt'),
+            '⌫': mac ?  t('shortcuts.key.delete') : t('shortcuts.key.backspace'),
+            '⌦': mac ?  t('shortcuts.key.del')    : t('shortcuts.key.del'),
         };
 
     for (var i = 0; i < code.length; i++) {
@@ -44,7 +43,7 @@ uiCmd.display = function(code) {
 
     var detected = utilDetect();
     var mac = (detected.os === 'mac');
-    
+
     var replacements = {
         '⌘': mac ? '⌘ ' + t('shortcuts.key.cmd')    : t('shortcuts.key.ctrl'),
         '⇧': mac ? '⇧ ' + t('shortcuts.key.shift')  : t('shortcuts.key.shift'),
